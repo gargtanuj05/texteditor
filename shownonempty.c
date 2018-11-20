82a961198895e6f7947bfc8c8b0bf2459d07798d
Егор <33566420+Hemmemm@users.noreply.github.com>
2018-11-20 13:00:01 +0300

    Add files via upload
    Функция shownonempty

---
+++ shownonempty.c
@@ -0,0 +1,51 @@
+/**
+ * shownonempty.c -- реализует команду вывода содержимого непустых строк
+ *
+ * Copyright (c) 2018, Egor Ignatov <ignatov@petrsu.ru>
+ *
+ * This code is licensed under a MIT-style license.
+ */
+
+#include <stdio.h>
+#include <assert.h>
+#include "common.h"
+#include "text/text.h"
+#include <stdlib.h>
+#include <ctype.h>
+
+#define RESET "\033[0m"
+
+static void shownonempty_line(int index, char* contents, int cursor, void* data);
+
+void shownonempty(text txt)
+{
+    process_forward(txt, shownonempty_line, NULL);
+}
+
+static void shownonempty_line(int index, char* contents, int cursor, void* data)
+{
+    /* Функция обработчик всегда получает существующую строку*/
+    assert(contents != NULL);
+
+    /* Декларируем неиспользуемые параметры*/
+    UNUSED(cursor);
+    UNUSED(data);
+    UNUSED(index);
+
+    /* Вывод непустых строк*/
+    if (contents[0] != '\0'){
+	int i = 1;
+	while(contents[i] != '\0'){
+	    /* Проверям является ли текущий символ пробелом(пробел,
+	     * табуляция, etc)*/
+	    if (isspace(contents[i])){
+		i++;
+		continue;
+	    }
+	    
+	    /* Если нашли непробельный символ, то выводим строку*/
+	    printf(RESET"%s", contents);
+	    break;
+	}
+    }
+}
