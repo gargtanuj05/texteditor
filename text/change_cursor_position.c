c2d2d6fc0053e12bc951e7141a9097047548a946
Егор <33566420+Hemmemm@users.noreply.github.com>
2018-12-03 22:51:03 +0300

    huging fixes (#15)

--- text/change_cursor_position.c
+++ text/change_cursor_position.c
@@ -26,23 +26,15 @@ void change_cursor_position(text txt, int new_line, int new_position)
 {
   new_line--;
   new_position--;
-  if (new_line < 0 || new_line > (int)txt->length){
+  if (new_line < 0 || new_line > (int)txt->length)
     new_line = 0;
-    if (new_line < 0 || new_line > (int)txt->length || new_position < 0){
-      return;
-    }
-    node *p;
-    p = txt->begin;
-    for (int i = 0; i < new_line; i++){
-      p = p->next;
-    }
-    if (new_position > (int)strlen(p->contents) || new_position < 0){
-      new_position = 0;
-      if (new_position > (int)strlen(p->contents)){
-        return;
-      }
-      txt->cursor->position = new_position;
-      txt->cursor->line = p;
-    }
+  node *p;
+  p = txt->begin;
+  for (int i = 0; i < new_line; i++){
+    p = p->next;
   }
+  if (new_position > (int)strlen(p->contents) || new_position < 0)
+    new_position = 0;
+  txt->cursor->position = new_position;
+  txt->cursor->line = p;
 }
